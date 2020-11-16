7878695a6f0b5504d0ccd2cec506dfbd6bafaab5
Alexey Kirinyuk <alexey.kirinyuk@gmail.com>
2020-11-16 21:05:04 +0300

    implemented ActivityDashboard

---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Form, Segment } from 'semantic-ui-react';
+
+
+
+const ActivityForm = () => {
+  return (
+    <Segment>
+      <Form>
+        <Form.Input placeholder="Title" />
+        <Form.TextArea rows={2} placeholder="Description" />
+        <Form.Input placeholder="Category" />
+        <Form.Input placeholder="Date" type="date" />
+        <Form.Input placeholder="City" />
+        <Form.Input placeholder="Venue" />
+      </Form>
+    </Segment>
+  )
+};
+
+export default ActivityForm;
